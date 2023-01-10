23b84fbcced194795489b6f72970798b29f29310
gabkhanfig <gabkhanfig@gmail.com>
2023-01-10 17:01:01 -0500

    Add GenerateFile script

---
+++ GenerateFile.py
@@ -0,0 +1,51 @@
+import sys
+
+'''
+Command line arguments.
+First argument is the name of the header file to generate. Will default to "generatedConstChars.h".
+Second argument is the text file containing the files that will be used to generate the header.
+'''
+
+headerFileName = None
+if(len(sys.argv) != 3):
+    exit()
+    #headerFileName = "generatedConstChars.h"
+
+
+headerFileName = sys.argv[1] + ".h"
+files = sys.argv[2]
+
+
+header = open(headerFileName, "w")
+header.write("#pragma once\n")
+
+gens = open(files, "r").read().split("\n")
+
+# Creates a string formatted as c++ constexpr const char* variableName = fileContents
+def FileToConstChar(filePath : str, variableName : str):
+    file = open(filePath, "r")
+    contents = file.read()
+    contents = "R\"(" + contents + ")\";"
+    file.close()
+    lastIndex = filePath.rfind("/")
+    fileName = filePath[lastIndex + 1 : len(filePath)]
+
+    return "// Generated from " + fileName + "\nconstexpr const char* " + variableName + " = " + contents
+
+# Append the const char* string to the header file
+def AppendConstCharToHeader(constChar : str):
+    header.write("\n")
+    header.write(constChar)
+    header.write("\n")
+
+for gen in gens:
+    print("Generating constexpr const char* string for file: " + gen)
+    lastIndex = gen.rfind("/")
+    fileName = gen[lastIndex + 1 : len(gen)]
+    typeIndex = gen.rfind(".")
+    fileStart = fileName[0 : typeIndex]
+    fileType = fileName[typeIndex + 1 : len(fileName)]
+    variableName = "generated_" + fileStart + "_" + fileType
+    AppendConstCharToHeader(FileToConstChar(gen, variableName))
+
+header.close()
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Cpp-Compile-Time-String-Generator
 Generates a c++ header file given the contents of chosen files with the file contents converted to constexpr const char*s
+
+```
+// Example of execution
+python GenerateFile.py shaders Shaders.txt
+```
+
+The txt file used should have the absolute or relative paths to the files.
+
+```
+// Shaders.txt
+C:/Users/SomePath/example.vert
+C:/Users/SomePath/example.frag
+```
+
+The corresponding files can be any file.
+
+```
+// example.vert
+#version 460 core
+layout (location = 0) in vec3 aPos;
+void main()
+{
+   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+
+// example.frag
+#version 460 core
+out vec4 FragColor;
+void main()
+{
+   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);
+}
+```
